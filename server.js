328138588577fe4e038f9dfb5aa524afcdb043c7
Pradeep M <pradeepmuthu1526@gmail.com>
2026-05-13 23:01:32 +0530

    modfied

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 
+// CORS
 app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-// routes
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected ✔"))
+  .catch((err) => console.log("DB Connection Error:", err));
+
+// Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/books", require("./routes/bookRoutes"));
 app.use("/api/borrow", require("./routes/borrowRoutes"));
 
+// Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
